6a945bd7c630a1504445d876df5602875f496c78
henri amani <henri.amani@mfp.ci>
2023-12-05 19:16:43 +0000

    Suppression des fichiers et package breeze, mise à jour des migrations

--- routes/api.php
+++ routes/api.php
@@ -15,14 +15,3 @@ use App\Http\Controllers\SubcategoriesController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-
-Route::resource('categories', CategoriesController::class);
-Route::resource('subcategories', SubcategoriesController::class);
-
-
-
-});
-
